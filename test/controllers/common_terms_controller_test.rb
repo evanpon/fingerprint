a874802091f8ebae6083e47f2f5ab4dc0c0467c7
Evan Pon <evanpon@gmail.com>
2017-03-26 17:56:44 -0700

    fix tests so they properly login as part of the test. Also add a test to make sure the login redirect works.

--- test/controllers/common_terms_controller_test.rb
+++ test/controllers/common_terms_controller_test.rb
@@ -6,16 +6,19 @@ class CommonTermsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
+    login
     get common_terms_url
     assert_response :success
   end
 
   test "should get new" do
+    login
     get new_common_term_url
     assert_response :success
   end
 
   test "should create common_term" do
+    login
     assert_difference('CommonTerm.count') do
       post common_terms_url, params: { common_term: { pages: @common_term.pages, search_term: @common_term.search_term } }
     end
@@ -24,25 +27,38 @@ class CommonTermsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show common_term" do
+    login
     get common_term_url(@common_term)
     assert_response :success
   end
 
   test "should get edit" do
+    login
     get edit_common_term_url(@common_term)
     assert_response :success
   end
 
   test "should update common_term" do
+    login
     patch common_term_url(@common_term), params: { common_term: { pages: @common_term.pages, search_term: @common_term.search_term } }
     assert_redirected_to common_term_url(@common_term)
   end
 
   test "should destroy common_term" do
+    login
     assert_difference('CommonTerm.count', -1) do
       delete common_term_url(@common_term)
     end
 
     assert_redirected_to common_terms_url
+  end
+  
+  test 'should redirect to login' do
+    get common_terms_url
+    assert_redirected_to login_path
+  end
+  
+  def login
+    post authenticate_path, params: {password: ENV['ADMIN_PASSWORD']}
   end
 end
