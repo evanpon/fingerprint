6ac7a8ef41ef659db41cace363ff48cc10b2e98a
Evan Pon <evanpon@gmail.com>
2017-03-26 17:37:02 -0700

    add some basic validations to common_term

--- app/models/common_term.rb
+++ app/models/common_term.rb
@@ -1,2 +1,11 @@
 class CommonTerm < ApplicationRecord
+  validates :search_term, presence: true
+  validates :pages, numericality: {only_integer: true, greater_than: 0}
+  
+  before_validation :strip_whitespace
+  
+  def strip_whitespace
+    self.search_term = search_term.strip
+    puts "search term now: #{search_term}."
+  end
 end
